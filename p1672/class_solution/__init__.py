bff7106a0018cbe7d3016a3fb31a82a15ba382ae
Yurie-Koga <yurie.koga.a@gmail.com>
2021-06-28 17:15:29 -0700

    no. 1672

--- p1672/class_solution/__init__.py
+++ p1672/class_solution/__init__.py
@@ -1,4 +1,22 @@
+from typing import List
+
+
 class Solution:
-    def print_text(self):
-        print(f'Hello World')
-        pass
+    def maximumWealth2(self, accounts: List[List[int]]) -> int:
+        """
+        Runtime: 52 ms, faster than 77.28% of Python3 online submissions for Richest Customer Wealth.
+        Memory Usage: 14.4 MB, less than 26.62% of Python3 online submissions for Richest Customer Wealth.
+        """
+        return max([sum(x) for x in accounts])
+
+    def maximumWealth1(self, accounts: List[List[int]]) -> int:
+        """
+        Runtime: 52 ms, faster than 77.28% of Python3 online submissions for Richest Customer Wealth.
+        Memory Usage: 14.1 MB, less than 82.77% of Python3 online submissions for Richest Customer Wealth.
+        """
+        # sum of each accounts
+        # get max
+        res = []
+        for i in range(len(accounts)):
+            res.append(sum(accounts[i]))
+        return max(res)
--- p1672/test.py
+++ p1672/test.py
@@ -1,26 +1,33 @@
 import unittest
 from collections import namedtuple
 
-from p import class_solution
+from p1672 import class_solution
 
 
 class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self.solution = class_solution.Solution()
-        self.Case = namedtuple('Case', 'nums target expect')
+        self.Case = namedtuple('Case', 'accounts expect')
         # example cases
         self.cases_example = [
-            self.Case([2, 7, 11, 15], 9, [0, 1]),
-            self.Case([3, 2, 4], 6, [1, 2]),
-            self.Case([3, 3], 6, [0, 1]),
+            self.Case([[1, 2, 3], [3, 2, 1]], 6),
+            self.Case([[1, 5], [7, 3], [3, 5]], 10),
+            self.Case([[2, 8, 7], [7, 1, 3], [1, 9, 5]], 17),
         ]
 
-    # twoSum1
-    # def test_examples1(self):
-    #     for i, Case in enumerate(self.cases_example):
-    #         with self.subTest(f'twoSum1: case: {i}'):
-    #             act = self.solution.print_text(Case.nums, Case.target)
-    #             self.assertEqual(act, Case.expect)
+    # maximumWealth1
+    def test_examples1(self):
+        for i, Case in enumerate(self.cases_example):
+            with self.subTest(f'maximumWealth1: case: {i}'):
+                act = self.solution.maximumWealth1(Case.accounts)
+                self.assertEqual(act, Case.expect)
+
+    # maximumWealth2
+    def test_examples2(self):
+        for i, Case in enumerate(self.cases_example):
+            with self.subTest(f'maximumWealth2: case: {i}'):
+                act = self.solution.maximumWealth2(Case.accounts)
+                self.assertEqual(act, Case.expect)
 
 
 if __name__ == '__main__':
